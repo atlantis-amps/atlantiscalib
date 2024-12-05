3a1d3ff620de928bd9bf42c5922e4953fd6841be
Hem Nalini Morzaria-Luna <hmorzaria@hotmail.com>
2024-12-05 12:08:19 -0800

    Corrected pprey matrix structure

--- R/modify_pprey.R
+++ R/modify_pprey.R
@@ -26,29 +26,55 @@ modify_pprey <- function(eachrow, biology.prm){
 
     names(pprey.index) <- pprey.cols
 
-    pprey.matrix <- biology.prm[17162:17846]
+    pprey.matrix <- biology.prm[17163:17848]
 
-    pprey.names <- grep("^pPREY",pprey.matrix, value = TRUE) %>%
+    pprey.names.all <- grep("^pPREY",pprey.matrix, value = TRUE) %>%
       gsub("\t","", .) %>%
       tibble::as_tibble() %>%
       tidyr::separate(value,into=c("pprey_name","entries"), sep=" ") %>%
-      dplyr::mutate(preystage= pprey_name) %>%
+      dplyr::mutate(preystage= pprey_name)
+
+    #separate into vertebrates and invertebrates, code was cutting off the invertebrate names because they don't have a number
+
+    pprey.names.vert <- pprey.names.all %>%
+      dplyr::filter(grepl("pPREY1", preystage)| grepl("pPREY2", preystage)) %>%
       tidyr::separate(preystage, into=c("ppreystage","ppredstage"), sep=6) %>%
-      dplyr::mutate(preystage = stringr::str_extract(ppreystage, "[0-9]")) %>%
-      dplyr::mutate(predstage = stringr::str_extract(ppredstage, "[0-9]")) %>%
+      dplyr::mutate(preystage = as.numeric(stringr::str_extract(ppreystage, "[0-9]"))) %>%
+      dplyr::mutate(predstage = as.numeric(stringr::str_extract(ppredstage, "[0-9]"))) %>%
       dplyr::mutate(pred = stringr::str_extract(ppredstage, "[:alpha:]+[ ?[:alpha:]]*"))
 
-    pprey.values <- pprey.matrix[!grepl("^pPREY", pprey.matrix)] %>%
+
+    pprey.names.invert <- pprey.names.all %>%
+      dplyr::filter(!str_detect(preystage, ("pPREY1"))) %>%
+      dplyr::filter(!str_detect(preystage, ("pPREY2"))) %>%
+      dplyr::mutate(pred = gsub("pPREY","",preystage), ppredstage = pred, preystage=1, predstage = 1, ppreystage = "pPREY") %>%
+      dplyr::select(pprey_name, entries, ppreystage, ppredstage, preystage, predstage, pred)
+
+    pprey.names <- dplyr::bind_rows(pprey.names.vert, pprey.names.invert)
+
+    pprey.values <- pprey.matrix %>%
       gsub("\t"," ", .) %>%
       tibble::as_tibble() %>%
       tidyr::separate(value,into=pprey.cols, sep=" ") %>%
-      dplyr::mutate_at(dplyr::vars(BB:DR_s), as.numeric) %>%
-      dplyr::filter(!is.na(BB)) %>%
+      dplyr::filter(!BB=="") %>%
+      dplyr::filter(!str_detect(BB, ("pPREY"))) %>%
+      dplyr::mutate_at(dplyr::vars(BB:DC_s), as.numeric) %>%
       dplyr::bind_cols(pprey.names, .) %>%
       dplyr::mutate(index = 1:nrow(.)) %>%
-      dplyr::select(index, everything()) %>%
-      dplyr::mutate(across(c(BB:DC_s), as.numeric))
+      dplyr::select(index, everything())
 
+    #create template for use in creating pprey matrix from diets
+
+    pprey.template <- pprey.matrix %>%
+      gsub("\t"," ", .) %>%
+      tibble::as_tibble() %>%
+      tidyr::separate(value,into=pprey.cols, sep=" ") %>%
+      dplyr::filter(!BB=="") %>%
+      dplyr::filter(!str_detect(BB, ("pPREY"))) %>%
+      dplyr::mutate_at(dplyr::vars(BB:DC_s), as.numeric) %>%
+      dplyr::mutate_if(is.numeric, ~0 * (. > 0))
+
+    readr::write_csv(pprey.template,here::here("data-raw","pprey_template.csv"))
 
     if(prey.mod == 1){
 
@@ -93,9 +119,9 @@ modify_pprey <- function(eachrow, biology.prm){
       print(pprey.vec[eachrow])
     }
 
-    biology.prm <- biology.prm[-c(17162:17846)]
+    biology.prm <- biology.prm[-c(17163:17848)]
 
-    biology.prm[17162:(17162+length(pprey.vec))] <- pprey.vec
+    biology.prm[17163:(17163+length(pprey.vec))] <- pprey.vec
 
     return(biology.prm)
   }
